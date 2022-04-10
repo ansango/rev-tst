5ff0e459c7b53c309c5ba21b664ee292a74d0e05
ansango <anibalsantosgo@gmail.com>
2022-04-10 17:52:26 +0200

    clenaed swr hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from "next";
 import BlockContainer from "components/common/Container/BlockContainer";
-import { useCurrentAccount } from "@/lib-client/hooks/account";
+
 import { useAppSelector } from "@/lib-client/store/hooks";
 import { selectUser } from "@/lib-client/store/features/user/userSlice";
+import { selectAccount } from "@/lib-client/store/features/account/accountSlice";
 
 const Home: NextPage = () => {
   const user = useAppSelector(selectUser);
-  const { data: { account } = {} } = useCurrentAccount();
+  const account = useAppSelector(selectAccount);
 
   return (
     <BlockContainer>
