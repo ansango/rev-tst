51240a28c75c8549761030c3981b0ccc889d7415
ansango <anibalsantosgo@gmail.com>
2022-04-13 02:10:01 +0200

    motion routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,26 @@ import WrapperLayout from "components/layout/WrapperLayout";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import store from "@/lib-client/store";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router: { route } }: AppProps) {
   return (
     <Provider store={store}>
-      <WrapperLayout>
-        <Component {...pageProps} />
-        <Toaster />
-      </WrapperLayout>
+      <motion.div
+        key={route}
+        initial="initial"
+        animate="animate"
+        variants={{
+          initial: { opacity: 0 },
+          animate: { opacity: 1 },
+        }}
+      >
+        <WrapperLayout>
+          <Component {...pageProps} />
+
+          <Toaster />
+        </WrapperLayout>
+      </motion.div>
     </Provider>
   );
 }
