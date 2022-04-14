7b6d65e0070bada89d893b67a74ea12926836860
ansango <anibalsantosgo@gmail.com>
2022-04-14 13:09:06 +0200

    checkbox

--- components/common/Forms/Checkbox/Checkbox.tsx
+++ components/common/Forms/Checkbox/Checkbox.tsx
@@ -23,8 +23,8 @@ const Checkbox: FC<CheckboxProps> = ({ label, name, options, link, ...rest }) =>
           type="checkbox"
           className={
             !errors[name]
-              ? "w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              : "w-4 h-4 text-red-600 bg-red-100 rounded border-red-300 focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              ? "w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2"
+              : "w-4 h-4 text-red-600 bg-red-100 rounded border-red-300 focus:ring-red-500 focus:ring-2"
           }
           {...register(name, { ...options })}
           {...rest}
@@ -34,8 +34,8 @@ const Checkbox: FC<CheckboxProps> = ({ label, name, options, link, ...rest }) =>
             htmlFor={name}
             className={
               !errors[name]
-                ? "ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                : "ml-3 text-sm font-medium text-red-700 dark:text-red-500"
+                ? "ml-3 text-sm font-medium text-gray-900"
+                : "ml-3 text-sm font-medium text-red-700"
             }
           >
             {label} {link}
