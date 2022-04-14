0666c4dc395b5578f550b6f4cee57da9bfe29340
ansango <anibalsantosgo@gmail.com>
2022-04-14 13:03:15 +0200

    select rmv dark

--- components/common/Forms/Select/Select.tsx
+++ components/common/Forms/Select/Select.tsx
@@ -23,8 +23,8 @@ const Select: FC<SelectProps> = ({ label, name, data, options, ...rest }) => {
           htmlFor={name}
           className={
             !errors[name]
-              ? "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              : "block mb-2 text-sm font-medium text-red-700 dark:text-red-500"
+              ? "block mb-2 text-sm font-medium text-gray-900"
+              : "block mb-2 text-sm font-medium text-red-700"
           }
         >
           {label}
@@ -33,8 +33,8 @@ const Select: FC<SelectProps> = ({ label, name, data, options, ...rest }) => {
       <select
         className={
           !errors[name]
-            ? "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            : "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400"
+            ? "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            : "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
         }
         {...register(name, { ...options })}
         {...rest}
@@ -46,7 +46,7 @@ const Select: FC<SelectProps> = ({ label, name, data, options, ...rest }) => {
         ))}
       </select>
       {errors[name] && (
-        <p className="mt-2 text-sm text-red-600 dark:text-red-500">{errors[name].message}</p>
+        <p className="mt-2 text-sm text-red-600">{errors[name].message}</p>
       )}
     </div>
   );
