d75057f028940174f9e39765f3a1668c409737c1
ansango <anibalsantosgo@gmail.com>
2022-04-14 13:07:09 +0200

    rmv dark file

--- components/common/Forms/File/File.tsx
+++ components/common/Forms/File/File.tsx
@@ -23,8 +23,8 @@ const File: FC<FileProps> = ({ label, name, options, ...rest }) => {
           htmlFor={name}
           className={
             !errors[name]
-              ? "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              : "block mb-2 text-sm font-medium text-red-700 dark:text-red-500"
+              ? "block mb-2 text-sm font-medium text-gray-900"
+              : "block mb-2 text-sm font-medium text-red-700"
           }
         >
           {label}
@@ -36,16 +36,14 @@ const File: FC<FileProps> = ({ label, name, options, ...rest }) => {
         type="file"
         className={
           !errors[name]
-            ? "file:mr-4 file:py-2.5 file:px-4 file:border-0 file:bg file:text-sm file:text-white file:bg-gray-700 file:hover:bg-gray-600 file:font-medium file:cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            : "file:mr-4 file:py-2.5 file:px-4 file:border-0 file:bg file:text-sm file:text-white file:bg-red-700 file:hover:bg-red-600 file:font-medium file:cursor-pointer bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500 block w-full dark:bg-red-100 dark:border-red-400"
+            ? "file:mr-4 file:py-2.5 file:px-4 file:border-0 file:bg file:text-sm file:text-white file:bg-gray-700 file:hover:bg-gray-600 file:font-medium file:cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 block w-full"
+            : "file:mr-4 file:py-2.5 file:px-4 file:border-0 file:bg file:text-sm file:text-white file:bg-red-700 file:hover:bg-red-600 file:font-medium file:cursor-pointer bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500 block w-full"
         }
         {...register(name, { ...options })}
         {...rest}
       />
       {errors[name] && (
-        <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-          {errors[name].message}
-        </p>
+        <p className="mt-2 text-sm text-red-600">{errors[name].message}</p>
       )}
     </div>
   );
